e6376759c9794f8f64450661ebe0d3fff0f2f030
zelvad <vladislav.zelenyk@scalemeup.com>
2024-07-28 01:14:46 +0300

    refactor: added handlers for not exist navigation

--- src/components/Layouts/RootLayout/Footer/FooterContainer.tsx
+++ src/components/Layouts/RootLayout/Footer/FooterContainer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { NavigationItemData } from '@/services/contentful/types/controllers/navigation/get-controller';
 import { getNavigationItem } from '@/services/contentful/controllers/navigation/get-controller';
 import Footer from '@/components/Layouts/RootLayout/Footer/Footer';
 
 export default async function FooterContainer(): Promise<React.ReactElement> {
-  const navigationData: NavigationItemData = await getNavigationItem('footer');
+  const navigationData = await getNavigationItem('footer');
+
+  if (!navigationData) {
+    return <Footer navigationData={{ socialLinks: [], links: [] }} />;
+  }
 
   return <Footer navigationData={navigationData} />;
 }
--- src/components/Layouts/RootLayout/Header/HeaderContainer.tsx
+++ src/components/Layouts/RootLayout/Header/HeaderContainer.tsx
@@ -1,10 +1,13 @@
-import { NavigationItemData } from '@/services/contentful/types/controllers/navigation/get-controller';
 import { getNavigationItem } from '@/services/contentful/controllers/navigation/get-controller';
 import Header from '@/components/Layouts/RootLayout/Header/Header';
 import React from 'react';
 
 export default async function HeaderContainer(): Promise<React.ReactElement> {
-  const navigationData: NavigationItemData = await getNavigationItem('header');
+  const navigationData = await getNavigationItem('header');
+
+  if (!navigationData) {
+    return <Header navigationData={{ socialLinks: [], links: [] }} />;
+  }
 
   return <Header navigationData={navigationData} />;
 }
